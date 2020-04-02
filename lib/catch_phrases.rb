1510c61d8185d607755f83f6a86c3fdb3f5d0c83
Nandita Venkat <nanditavenkat@gmail.com>
2020-04-02 18:50:33 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -2,6 +2,7 @@ def mario
   
   phrase ="It's-a me, Mario!"
   puts phrase
+  
 end
 
 def toadstool
@@ -11,7 +12,11 @@ end
 
 def all_phrases
   
+  phrase ="It's-a me, Mario!"
+  puts phrase
   
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
+  
+  here =
   
